edee1373a606410662b585f97f4e27ab34b93dd2
Leonheart <33399712+bleonheart@users.noreply.github.com>
2024-06-19 18:13:08 +0100

    Updated Spacing

--- fallout/schema/factions/custom.lua
+++ fallout/schema/factions/custom.lua
@@ -1,6 +1,5 @@
 ﻿--- Attributes of the custom faction.
 -- @factions Custom
-
 --- A list of the current fields in the faction.
 -- @realm shared
 -- @table Configurations
--- fallout/schema/factions/default.lua
+++ fallout/schema/factions/default.lua
@@ -1,6 +1,5 @@
 ﻿--- Attributes of the default faction.
 -- @factions Default
-
 --- A list of the current fields in the faction.
 -- @realm shared
 -- @table Configurations
