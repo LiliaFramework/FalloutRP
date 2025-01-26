c2a6bfbcf3cc601a127ebcab6024598bb3b71d1d
Samael <33399712+bleonheart@users.noreply.github.com>
2025-01-26 06:22:19 +0000

    Cleanup

--- fallout/schema/schema.lua
+++ fallout/schema/schema.lua
@@ -1,8 +1,3 @@
-﻿--[[--
-    Schema configuration options.
-
-    This is meant to define the Schema's default information.
-]]
-SCHEMA.name = "FalloutRP"
+﻿SCHEMA.name = "FalloutRP"
 SCHEMA.author = "Samael"
 SCHEMA.desc = "A FalloutRP"
